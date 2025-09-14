30d9437da32bf59eeb9e36c984b3d455f87b15b3
PatcharapholToopprasom <bignorth46@gmail.com>
2025-09-14 14:03:52 +0700

    add doctor service and update readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppointmentModule } from './appointment/appointment.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { DoctorModule } from './doctor/doctor.module';
 
 @Module({
-  imports: [AppointmentModule, PrismaModule],
+  imports: [AppointmentModule, PrismaModule, DoctorModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({stopAtFirstError: true,}));
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true, 
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // exact origins
+    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    credentials: false,          // set true only if you use cookies
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
   await app.listen(process.env.PORT ?? 3001);
